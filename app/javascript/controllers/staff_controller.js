517657e5cf5d3bc47e53be06b66be8514a842031
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2023-12-07 18:36:06 +1300

    wire up form target

--- app/javascript/controllers/staff_controller.js
+++ app/javascript/controllers/staff_controller.js
@@ -1,11 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["form"];
   connect() {
     console.log("Range slider connected");
   }
 
   change(event) {
     console.log("Range slider value changed to", event.target.value);
+    // submit the parent form
+    console.log(this.formTarget);
   }
 }
