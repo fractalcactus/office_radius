29d4153037ae217c2c8544c035539043d8c599ef
fractalcactus <8527715+fractalcactus@users.noreply.github.com>
2023-12-07 22:20:14 +1300

    bug fixes

--- app/controllers/staff_controller.rb
+++ app/controllers/staff_controller.rb
@@ -7,6 +7,6 @@ class StaffController < ApplicationController
 
   def within_radius
     @max = params[:max].to_f || 500.0
-    @staff = ProcessStaff.process(@max, "/Users/gabrielleyoung/Documents/office_radius/app/services/staff_list.json")
+    @staff = ProcessStaff.process(@max, Rails.root.join("app","services","staff_list.json").to_s)
   end
 end
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,7 +4,5 @@
 
 import { application } from "./application";
 
-import HelloController from "./hello_controller";
 import StaffController from "./staff_controller.js";
-application.register("hello", HelloController);
 application.register("staff", StaffController);
